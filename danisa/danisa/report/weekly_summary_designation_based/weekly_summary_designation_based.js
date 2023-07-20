2b9826f7ed64ea5e63d441cae2372f392a308c57
maveeshah <mavee.shah@hotmail.com>
2023-07-20 21:05:35 +0500

    Weekly report

---
+++ danisa/danisa/report/weekly_summary_designation_based/weekly_summary_designation_based.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2023, Ameer Muavia Shah and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Weekly Summary Designation Based"] = {
+  filters: [
+    {
+      fieldname: "company",
+      label: __("Company"),
+      fieldtype: "Link",
+      options: "Company",
+    },
+    {
+      fieldname: "from_date",
+      label: __("From Date"),
+      fieldtype: "Date",
+      reqd: 1,
+    },
+    {
+      fieldname: "to_date",
+      label: __("To Date"),
+      fieldtype: "Date",
+      reqd: 1,
+    },
+    {
+		fieldname: "designation",
+		label: __("Designation"),
+		fieldtype: "Link",
+		options: "Designation",
+	  },
+  ],
+};
---
+++ danisa/danisa/report/weekly_summary_designation_based/weekly_summary_designation_based.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2023, Ameer Muavia Shah and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.utils import getdate,cstr
+from datetime import timedelta,datetime
+from frappe import _
+
+
+
+def execute(filters=None):
+	if not filters:
+		filters = {}
+	from_date = getdate(filters.get("from_date"))
+	to_date = getdate(filters.get("to_date"))
+	
+	date_list = []
+	current_date = from_date
+	while current_date <= to_date:
+		date_list.append(current_date)
+		current_date += timedelta(days=1)
+
+	columns, data = [], []
+	columns = get_columns(date_list)
+	conds = get_conds(filters)
+	data = get_results(filters,date_list,conds)
+	return columns, data
+
+
+def get_conds(filters):
+	conds = ""
+	conds += " AND ompany = %(company)s " if filters.get("company") else ""
+	# conds += " AND e.designation = %(designation)s " if filters.get("designation") else ""
+	conds += " AND attendance_date between %(from_date)s AND %(to_date)s " if filters.get("from_date") and filters.get("to_date") else ""
+	return conds
+
+def get_emp_conds(filters):
+	conds = ""
+	conds += " AND designation = %(designation)s " if filters.get("designation") else ""
+	return conds
+
+
+def get_columns(date_list):
+	columns = [ _("Employee Name") + "::90", _("ID. No.") + "::150",_("Phone No") + "::150"]
+	for day in range(len(date_list)):
+		columns.append(cstr(day+1) +"::20")
+	columns += [_("Amount") + ":Currency:95"]
+	return columns
+
+def get_results(filters,date_list,conds):
+	att_map = get_attendance_list(conds, filters)
+	emp_map = get_employee_details(filters)
+	data = []
+	amount = 0
+	for emp in sorted(att_map):
+		emp_det = emp_map.get(emp)
+		if not emp_det:
+			continue
+		row = [emp_det.employee_name, emp_det.designation, emp_det.cell_number]
+		for date in date_list:
+			status = att_map.get(emp).get(date, ["None"])
+			if  status[0] == "Present":
+				row.append(1)
+				amount = amount + 720
+			else:
+				row.append(0)
+		row.append(amount)	
+	data.append(row)
+	return data
+			 
+def get_attendance_list(conditions, filters):
+
+	attendance_list = frappe.db.sql("""select employee, attendance_date as day_of_month,
+		status from tabAttendance 
+		where docstatus = 1 %s order by employee, attendance_date""" % conditions, filters, as_dict=1)
+
+	att_map = {}
+	for d in attendance_list:
+		att_map.setdefault(d.employee, frappe._dict()).setdefault(d.day_of_month, "")
+		att_map[d.employee][d.day_of_month] = [d.status]
+
+	return att_map
+
+def get_employee_details(filters):
+	emp_conds = get_emp_conds(filters)
+	emp_map = frappe._dict()
+	for d in frappe.db.sql("""select name, employee_name, branch, designation,cell_number,company
+		from tabEmployee where status = 'Active' %s """  % emp_conds, filters, as_dict=1):
+		emp_map.setdefault(d.name, d)
+
+	return emp_map
