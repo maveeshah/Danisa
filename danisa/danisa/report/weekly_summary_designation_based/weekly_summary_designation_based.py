2bdbe8652e4abda49205b81c7a270df850c02618
maveeshah <mavee.shah@hotmail.com>
2023-07-22 20:54:07 +0500

    changes in report

--- danisa/danisa/report/weekly_summary_designation_based/weekly_summary_designation_based.py
+++ danisa/danisa/report/weekly_summary_designation_based/weekly_summary_designation_based.py
@@ -29,63 +29,61 @@ def execute(filters=None):
 
 def get_conds(filters):
 	conds = ""
-	conds += " AND ompany = %(company)s " if filters.get("company") else ""
-	# conds += " AND e.designation = %(designation)s " if filters.get("designation") else ""
-	conds += " AND attendance_date between %(from_date)s AND %(to_date)s " if filters.get("from_date") and filters.get("to_date") else ""
-	return conds
-
-def get_emp_conds(filters):
-	conds = ""
+	conds += " AND company = %(company)s " if filters.get("company") else ""
 	conds += " AND designation = %(designation)s " if filters.get("designation") else ""
+	conds += " AND attendance_date between %(from_date)s AND %(to_date)s " if filters.get("from_date") and filters.get("to_date") else ""
 	return conds
 
 
 def get_columns(date_list):
-	columns = [ _("Employee Name") + "::90", _("ID. No.") + "::150",_("Phone No") + "::150"]
-	for day in range(len(date_list)):
-		columns.append(cstr(day+1) +"::20")
-	columns += [_("Amount") + ":Currency:95"]
+	columns = [ _("Employee Name") + "::190", _("ID. No.") + "::150",_("Phone No") + "::150"]
+	for day in date_list:
+		#columns.append(cstr(day+1) +"::20")
+		columns.append(day.strftime("%a") + "::70")
+	columns += [_("Total Shifts") +":Int:80",_("Amount") + ":Currency:95"]
 	return columns
 
 def get_results(filters,date_list,conds):
 	att_map = get_attendance_list(conds, filters)
 	emp_map = get_employee_details(filters)
 	data = []
-	amount = 0
 	for emp in sorted(att_map):
+		amount = total_shifts = 0
 		emp_det = emp_map.get(emp)
 		if not emp_det:
 			continue
-		row = [emp_det.employee_name, emp_det.designation, emp_det.cell_number]
+		row = [emp_det.employee_name, emp_det.id_number, emp_det.cell_number]
 		for date in date_list:
 			status = att_map.get(emp).get(date, ["None"])
 			if  status[0] == "Present":
 				row.append(1)
-				amount = amount + 720
+				per_shift =  frappe.db.get_value("Designation",status[1], "amount_per_shift")
+				amount = amount  + per_shift if per_shift else 0
+				total_shifts += 1
 			else:
 				row.append(0)
+		row.append(total_shifts)
 		row.append(amount)	
-	data.append(row)
+		data.append(row)
 	return data
 			 
 def get_attendance_list(conditions, filters):
-
 	attendance_list = frappe.db.sql("""select employee, attendance_date as day_of_month,
-		status from tabAttendance 
+		amount,	status, designation from tabAttendance 
 		where docstatus = 1 %s order by employee, attendance_date""" % conditions, filters, as_dict=1)
 
 	att_map = {}
 	for d in attendance_list:
 		att_map.setdefault(d.employee, frappe._dict()).setdefault(d.day_of_month, "")
-		att_map[d.employee][d.day_of_month] = [d.status]
+		att_map[d.employee][d.day_of_month] = [d.status,d.designation]
 
 	return att_map
 
 def get_employee_details(filters):
-	emp_conds = get_emp_conds(filters)
+	emp_conds = " "
 	emp_map = frappe._dict()
-	for d in frappe.db.sql("""select name, employee_name, branch, designation,cell_number,company
-		from tabEmployee where status = 'Active' %s """  % emp_conds, filters, as_dict=1):
+	for d in frappe.db.sql("""select name, employee_name, branch, id_number,cell_number,company,
+		designation from tabEmployee where status = 'Active' %s """  % emp_conds, filters, as_dict=1):
 		emp_map.setdefault(d.name, d)
 
 	return emp_map
