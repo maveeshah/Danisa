bf5fb111b73276211b09a98fdd0b7314ff6263f5
maveeshah <mavee.shah@hotmail.com>
2023-07-23 22:03:49 +0500

    Changes in reports

--- danisa/danisa/report/weekly_attendance_sheet/weekly_attendance_sheet.js
+++ danisa/danisa/report/weekly_attendance_sheet/weekly_attendance_sheet.js
@@ -32,42 +32,4 @@ frappe.query_reports["Weekly Attendance Sheet"] = {
       options: "Designation",
     },
   ],
-  onload: function (report) {
-    const from_date_field = frappe.query_report.get_filter("from_date");
-    const to_date_field = frappe.query_report.get_filter("to_date");
-
-    if (from_date_field) {
-      from_date_field.$input.on("change", function () {
-        validateDateRange();
-      });
-    }
-
-    if (to_date_field) {
-      to_date_field.$input.on("change", function () {
-        validateDateRange();
-      });
-    }
-  },
 };
-
-function validateDateRange() {
-  const from_date = frappe.query_report.get_filter_value("from_date");
-  const to_date = frappe.query_report.get_filter_value("to_date");
-
-  const fromDateObj = new Date(from_date);
-  const toDateObj = new Date(to_date);
-  const maxDateDifference = 7; // Maximum allowed difference between dates
-
-  // Compare dates and show error message if invalid
-  if (
-    fromDateObj > toDateObj ||
-    (toDateObj - fromDateObj) / (1000 * 60 * 60 * 24) > maxDateDifference
-  ) {
-    frappe.msgprint(
-      __(
-        "Invalid date range. The 'To Date' must be greater than or equal to 'From Date' and the difference between the two dates cannot be greater than 7 days."
-      )
-    );
-    frappe.query_report.set_filter_value("to_date", frappe.datetime.nowdate());
-  }
-}
--- danisa/danisa/report/weekly_designation_summary/weekly_designation_summary.js
+++ danisa/danisa/report/weekly_designation_summary/weekly_designation_summary.js
@@ -32,47 +32,5 @@ frappe.query_reports["Weekly Designation Summary"] = {
       options: "Shift Type",
     },
   ],
-  onload: function (report) {
-    // Add event listeners to the date filters (one() instead of on())
-    const from_date_field = frappe.query_report.get_filter("from_date");
-    const to_date_field = frappe.query_report.get_filter("to_date");
-
-    if (from_date_field) {
-      from_date_field.$input.one("change", function () {
-        validateDateRange();
-      });
-    }
-
-    if (to_date_field) {
-      to_date_field.$input.one("change", function () {
-        validateDateRange();
-      });
-    }
-  },
 };
 
-function validateDateRange() {
-  const from_date = frappe.query_report.get_filter_value("from_date");
-  const to_date = frappe.query_report.get_filter_value("to_date");
-
-  const fromDateObj = new Date(from_date);
-  const toDateObj = new Date(to_date);
-  const maxDateDifference = 7; // Maximum allowed difference between dates
-
-  // Compare dates and show error message if invalid
-  if (
-    fromDateObj > toDateObj ||
-    (toDateObj - fromDateObj) / (1000 * 60 * 60 * 24) > maxDateDifference
-  ) {
-    frappe.msgprint(
-      __(
-        "Invalid date range. The 'To Date' must be greater than or equal to 'From Date' and the difference between the two dates cannot be greater than 7 days."
-      )
-    );
-    frappe.query_report.set_filter_value(
-      "from_date",
-      frappe.datetime.add_days(frappe.datetime.nowdate(), -7)
-    );
-    frappe.query_report.set_filter_value("to_date", frappe.datetime.nowdate());
-  }
-}
