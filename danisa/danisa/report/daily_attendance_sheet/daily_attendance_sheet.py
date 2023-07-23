57b987334a53fb938764cc824ebc5e6dd08763ea
maveeshah <mavee.shah@hotmail.com>
2023-07-23 16:12:06 +0500

    Added Another report Weekly Attendance Sheet

--- danisa/danisa/report/daily_attendance_sheet/daily_attendance_sheet.py
+++ danisa/danisa/report/daily_attendance_sheet/daily_attendance_sheet.py
@@ -20,6 +20,7 @@ def get_conditions(filters):
 	conds = ""
 	conds += " AND company = %(company)s " if filters.get("company") else ""
 	conds += " AND attendance_date = %(attendance_date)s " if filters.get("attendance_date") else ""
+	conds += " AND designation = %(designation)s " if filters.get("designation") else ""
 	return conds
 
 def get_columns():
