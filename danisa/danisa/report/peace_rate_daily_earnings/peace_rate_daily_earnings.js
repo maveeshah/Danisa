653ab7a115cc2f3f6fd396e57d15c6ccd4b01ba0
maveeshah <mavee.shah@hotmail.com>
2023-07-26 21:48:08 +0500

    New reports regarding peace rate earnings

---
+++ danisa/danisa/report/peace_rate_daily_earnings/peace_rate_daily_earnings.js
@@ -0,0 +1,44 @@
+// Copyright (c) 2023, Ameer Muavia Shah and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Peace Rate Daily Earnings"] = {
+	filters: [
+		{
+		  fieldname: "company",
+		  label: __("Company"),
+		  fieldtype: "Link",
+		  options: "Company",
+		  reqd: 1,
+		},
+		{
+		  fieldname: "date",
+		  label: __("Date"),
+		  fieldtype: "Date",
+		  default: frappe.datetime.nowdate(),
+		  reqd: 1,
+		},
+		{
+		  fieldname: "employee_group",
+		  label: __("Employee Group"),
+		  fieldtype: "Link",
+		  options: "Employee Group",
+		  reqd: 1,
+		},
+		{
+		  fieldname: "designation",
+		  label: __("Designation"),
+		  fieldtype: "Link",
+		  options: "Designation",
+		  reqd: 1,
+				get_query: () => {
+					var company = frappe.query_report.get_filter_value('company');
+					return {
+						filters: {
+							'company': company
+						}
+					}
+				}
+		},
+	  ],
+};
