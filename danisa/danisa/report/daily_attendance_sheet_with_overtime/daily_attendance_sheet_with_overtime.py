66a1b9362fdbeb6c46bde689d6c9b5a23f1e04e8
maveeshah <mavee.shah@hotmail.com>
2023-08-16 05:28:04 +0500

    Testing Errors

--- danisa/danisa/report/daily_attendance_sheet_with_overtime/daily_attendance_sheet_with_overtime.py
+++ danisa/danisa/report/daily_attendance_sheet_with_overtime/daily_attendance_sheet_with_overtime.py
@@ -17,7 +17,7 @@ def execute(filters=None):
 	for attendance in att:
 		res_time = frappe.db.get_value("Designation",attendance.designation,"rest_time")
 		if res_time:
-			frappe.db.update("Attendance",attendance.name,"rest_time",res_time[0].rest_time)
+			frappe.db.update("Attendance",attendance.name,"rest_time",res_time)
 		else:
 			frappe.db.update("Attendance",attendance.name,"rest_time",0)
 
@@ -34,6 +34,7 @@ def get_conditions(filters):
 	conds += " AND company = %(company)s " if filters.get("company") else ""
 	conds += " AND attendance_date = %(attendance_date)s " if filters.get("attendance_date") else ""
 	conds += " AND designation = %(designation)s " if filters.get("designation") else ""
+	conds += " AND department = %(department)s " if filters.get("department") else ""
 	conds += " AND employee_group	 = %(employee_group)s " if filters.get("employee_group") else ""
 	return conds
 
@@ -42,6 +43,46 @@ def get_columns():
 	
 
 def get_results(filters,conditions):
+
+	# query = """
+	# 		SELECT 
+	# 			employee_name, 
+	# 			id_number, 
+	# 			shift, 
+	# 			place_of_work,
+	# 			SUBSTRING(time(in_time), 1, 5) AS in_time, 
+	# 			SUBSTRING(time(out_time), 1, 5) AS out_time,
+	# 			CASE 
+	# 				WHEN ADDTIME(TIMEDIFF(out_time, in_time), '-08:00:00') < '00:00:00' THEN '0'
+	# 				ELSE 
+	# 					FORMAT(
+	# 						FLOOR(
+	# 							TIME_TO_SEC(
+	# 								ADDTIME(TIMEDIFF(out_time, in_time), '-08:00:00')
+	# 							) / 3600
+	# 						) + 
+	# 						FLOOR(
+	# 							MOD(
+	# 								TIME_TO_SEC(
+	# 									ADDTIME(TIMEDIFF(out_time, in_time), '-08:00:00')
+	# 								),
+	# 								3600
+	# 							) / 1800
+	# 						) * 0.5 +
+	# 						FLOOR(
+	# 							MOD(
+	# 								TIME_TO_SEC(
+	# 									ADDTIME(TIMEDIFF(out_time, in_time), '-08:00:00')
+	# 								),
+	# 								1800
+	# 							) / 900
+	# 						) * 0.25,
+	# 						2
+	# 					)
+	# 			END AS overtime
+	# 		FROM `tabAttendance`
+	# 		WHERE docstatus = 1 {0}
+	# """.format(conditions,filters)
 
 	query = """
 			SELECT 
@@ -75,7 +116,8 @@ def get_results(filters,conditions):
 									),
 									1800
 								) / 900
-							) * 0.25,
+							) * 0.25
+							- rest_time,
 							2
 						)
 				END AS overtime
